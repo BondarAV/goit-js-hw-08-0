bc20f111d53de2edfbe910b589c0b433cc9d658f
Andrii Bondar <77549085+BondarAV@users.noreply.github.com>
2023-05-28 04:34:07 +0200

    Completes task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,64 @@
+const form = document.querySelector(".feedback-form");
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
 
+if (JSON.parse(localStorage.getItem("feedback-form-state")) === null) {
+  let storageObject = {
+    email: "",
+    message: "",
+  };
+
+  form.addEventListener("input", (event) => {
+    storageObject[event.target.name] = event.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(storageObject));
+  });
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+
+    console.log(formData);
+
+    localStorage.removeItem("feedback-form-state");
+    storageObject.email = "";
+    storageObject.message = "";
+
+    emailInput.value = "";
+    messageInput.value = "";
+  });
+} else {
+  let storageObject = JSON.parse(localStorage.getItem("feedback-form-state"));
+
+  function setDefaultInput(inputType, inputName) {
+    document.querySelector(`${inputType}[name="${inputName}"]`).defaultValue =
+      JSON.parse(localStorage.getItem("feedback-form-state"))[inputName];
+  }
+
+  setDefaultInput("input", "email");
+  setDefaultInput("textarea", "message");
+
+  form.addEventListener("input", (event) => {
+    storageObject[event.target.name] = event.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(storageObject));
+  });
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+
+    console.log(formData);
+
+    localStorage.removeItem("feedback-form-state");
+    storageObject.email = "";
+    storageObject.message = "";
+
+    emailInput.value = "";
+    messageInput.value = "";
+  });
+}
